8b354b63a3015dbcd0e92186bdfaed8589e5ee6f
Connor Michalec <connormichalec@gmail.com>
2022-06-30 04:20:08 -0500

    Removed auto-cpufreq install as TLP should handle battery power management

--- setup.sh
+++ setup.sh
@@ -135,10 +135,6 @@ echo "install symbola font(through yay) so braille characters work in urxvt"
 yay -S ttf-symbola --noconfirm
 
 echo ""
-echo "install auto-cpufreq(through yay) to make laptop use less battery"
-yay -S auto-cpufreq --noconfirm
-
-echo ""
 echo "install ble(through yay) for better terminal formatting"
 yay -S blesh --noconfirm
 
