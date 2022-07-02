85ea7039c6003a1664cf045f049d63d21645e636
Connor Michalec <connormichalec@gmail.com>
2022-07-02 18:03:04 -0500

    laptop screen rotatoin threshold increased to disable accidentaly rotation

--- xserver_rc.sh
+++ xserver_rc.sh
@@ -22,7 +22,7 @@ lxpolkit &
 flameshot &
 
 #start rot8 for screen rotatoin, you may have to edit the touchescreen input devices appropriately with xinput --list in order for the input devices to rotate
-rot8 --touchscreen "Wacom HID 5308 Finger" "Wacom HID 5308 Pen Pen (0x81a19ebc)" &
+rot8 --touchscreen "Wacom HID 5308 Finger" "Wacom HID 5308 Pen Pen (0x81a19ebc)" --threshold 0.8 &
 
 #start dwm
 exec dwm
